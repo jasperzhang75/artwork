606c3862634226715f1bd4bad3661102d9588e3a
jasperzhang75 <jasper.chang.chn@gmail.com>
2024-06-19 21:35:32 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Artwork from './components/Artwork/Artwork.jsx'
 import { Routes, Route } from "react-router-dom";
 import ArtworkDetail from './components/Artworkdetail/ArtworkDetail.jsx';
 import Favourites from './components/Favourites/Favourites.jsx';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/navbar.jsx';
 import { useState } from 'react';
 
 
