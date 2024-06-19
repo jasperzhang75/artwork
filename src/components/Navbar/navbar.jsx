360a59e0d25e61420e43fd41d00fe05da26d56a8
jasperzhang75 <jasper.chang.chn@gmail.com>
2024-06-19 21:37:24 +0200

    update

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import "./Navbar.css"
+import "./navbar.css"
 
 
 function NavBar({ onSearch }) {
